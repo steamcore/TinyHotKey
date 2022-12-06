4dd52f3ef0610a65852fb5cf733cb3d4ecc7a9f9
David Zidar <david@zidar.se>
2022-12-06 01:17:36 +0100

    Rename AddTinyHotKeys to AddTinyHotKey to reflect the project name

--- src/TinyHotKey/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/TinyHotKey/DependencyInjection/ServiceCollectionExtensions.cs
@@ -8,7 +8,7 @@ public static class ServiceCollectionExtensions
 	/// <para>Add a TinyHotKey singleton instance to the service collection.</para>
 	/// <para>Resolve ITinyHotKey and call RegisterHotKey to use hotkey detection.</para>
 	/// </summary>
-	public static IServiceCollection AddTinyHotKeys(this IServiceCollection services)
+	public static IServiceCollection AddTinyHotKey(this IServiceCollection services)
 	{
 		return services.AddSingleton<ITinyHotKey, TinyHotKeyInstance>();
 	}
