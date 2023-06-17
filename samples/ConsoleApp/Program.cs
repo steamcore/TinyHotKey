2ddd272f203ed11547c088dfe21ed154589c03aa
David Zidar <david@zidar.se>
2023-06-17 17:26:37 +0200

    Add comments to sample application

--- samples/ConsoleApp/Program.cs
+++ samples/ConsoleApp/Program.cs
@@ -6,8 +6,12 @@ public class Program
 {
 	public static void Main()
 	{
+		// Create a new instance of TinyHotKey, this contains the hidden Window and the message loop.
+		// Dispose of it when the application is shutting down.
 		using var tinyHotKey = new TinyHotKeyInstance();
 
+		// Register a hotkey, this returns a registration object that can be used to unregister the hotkey.
+		// Dispose of it when the hotkey is no longer needed.
 		using var registration = tinyHotKey.RegisterHotKey(Modifier.Control | Modifier.Alt, Key.D, () =>
 		{
 			Console.WriteLine("Ctrl+Alt+D detected");
@@ -15,6 +19,7 @@ public class Program
 			return Task.CompletedTask;
 		});
 
+		// Hotkey registration can fail if the hotkey is already registered by another application.
 		if (!registration.IsRegistered)
 		{
 			Console.WriteLine("Couldn't register hotkey Ctrl+Alt+D, maybe it's already taken by something else?");
