56f40a72e75f7b3d5f8ca87462da7a4ac1af7b53
David Zidar <david@zidar.se>
2023-09-18 23:30:54 +0200

    Use top level statements in sample program

--- samples/ConsoleApp/Program.cs
+++ samples/ConsoleApp/Program.cs
@@ -1,34 +1,26 @@
 using TinyHotKey;
 
-namespace ConsoleApp;
+// Create a new instance of TinyHotKey, this contains the hidden Window and the message loop.
+// Dispose of it when the application is shutting down.
+using var tinyHotKey = new TinyHotKeyInstance();
 
-public class Program
+// Register a hotkey, this returns a registration object that can be used to unregister the hotkey.
+// Dispose of it when the hotkey is no longer needed.
+using var registration = tinyHotKey.RegisterHotKey(Modifier.Control | Modifier.Alt, Key.D, () =>
 {
-	public static void Main()
-	{
-		// Create a new instance of TinyHotKey, this contains the hidden Window and the message loop.
-		// Dispose of it when the application is shutting down.
-		using var tinyHotKey = new TinyHotKeyInstance();
+	Console.WriteLine("Ctrl+Alt+D detected");
 
-		// Register a hotkey, this returns a registration object that can be used to unregister the hotkey.
-		// Dispose of it when the hotkey is no longer needed.
-		using var registration = tinyHotKey.RegisterHotKey(Modifier.Control | Modifier.Alt, Key.D, () =>
-		{
-			Console.WriteLine("Ctrl+Alt+D detected");
+	return Task.CompletedTask;
+});
 
-			return Task.CompletedTask;
-		});
+// Hotkey registration can fail if the hotkey is already registered by another application.
+if (!registration.IsRegistered)
+{
+	Console.WriteLine("Couldn't register hotkey Ctrl+Alt+D, maybe it's already taken by something else?");
 
-		// Hotkey registration can fail if the hotkey is already registered by another application.
-		if (!registration.IsRegistered)
-		{
-			Console.WriteLine("Couldn't register hotkey Ctrl+Alt+D, maybe it's already taken by something else?");
-
-			return;
-		}
-
-		Console.WriteLine("Hello HotKey, press Ctrl+Alt+D anywhere or enter here to quit");
-
-		Console.ReadLine();
-	}
+	return;
 }
+
+Console.WriteLine("Hello HotKey, press Ctrl+Alt+D anywhere or enter here to quit");
+
+Console.ReadLine();
