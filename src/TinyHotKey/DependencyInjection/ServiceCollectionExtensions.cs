c5bb8e66a000db228713ad92d14f19adb44c75fc
David Zidar <david@zidar.se>
2025-11-15 19:10:21 +0100

    Use extension members

--- src/TinyHotKey/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/TinyHotKey/DependencyInjection/ServiceCollectionExtensions.cs
@@ -4,12 +4,15 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class ServiceCollectionExtensions
 {
-	/// <summary>
-	/// <para>Add a TinyHotKey singleton instance to the service collection.</para>
-	/// <para>Resolve ITinyHotKey and call RegisterHotKey to use hotkey detection.</para>
-	/// </summary>
-	public static IServiceCollection AddTinyHotKey(this IServiceCollection services)
+	extension(IServiceCollection services)
 	{
-		return services.AddSingleton<ITinyHotKey, TinyHotKeyInstance>();
+		/// <summary>
+		/// <para>Add a TinyHotKey singleton instance to the service collection.</para>
+		/// <para>Resolve ITinyHotKey and call RegisterHotKey to use hotkey detection.</para>
+		/// </summary>
+		public IServiceCollection AddTinyHotKey()
+		{
+			return services.AddSingleton<ITinyHotKey, TinyHotKeyInstance>();
+		}
 	}
 }
--- src/TinyHotKey/ITinyHotKey.cs
+++ src/TinyHotKey/ITinyHotKey.cs
@@ -20,38 +20,40 @@ public interface ITinyHotKey
 
 public static class TinyHotKeyExtensions
 {
-	/// <summary>
-	/// <para>Register a callback to be invoke when a keyboard combination is detected.</para>
-	/// <para>
-	/// Since only one application at a time may listen to a certain keyboard combination on Windows
-	/// this method will throw an error if the registration was not a success.
-	/// </para>
-	/// <para>Multiple registrations for the same keyboard combination with the same instance is ok.</para>
-	/// </summary>
-	/// <param name="tinyHotKey">A TinyHotKey instance</param>
-	/// <param name="modifiers">Modifier keys, eg. Modifier.Ctrl | Modifier.Alt or Modifier.None</param>
-	/// <param name="key">Key to detect, eg. Key.C or Key.MediaPlayPause</param>
-	/// <param name="callback">Callback to be invoked when the keyboard combination is detected</param>
-	/// <returns>Registration status, should always be successful with this overload, dispose when done to remove the hotkey detection.</returns>
-	/// <exception cref="InvalidOperationException">Thrown when the hotkey combination could not be registered.</exception>
-	public static ITinyHotKeyRegistration RegisterHotKeyOrThrow(this ITinyHotKey tinyHotKey, Modifier modifiers, Key key, Func<Task> callback)
+	extension(ITinyHotKey tinyHotKey)
 	{
-#if NET
-		ArgumentNullException.ThrowIfNull(tinyHotKey);
-#else
-		if (tinyHotKey is null)
+		/// <summary>
+		/// <para>Register a callback to be invoke when a keyboard combination is detected.</para>
+		/// <para>
+		/// Since only one application at a time may listen to a certain keyboard combination on Windows
+		/// this method will throw an error if the registration was not a success.
+		/// </para>
+		/// <para>Multiple registrations for the same keyboard combination with the same instance is ok.</para>
+		/// </summary>
+		/// <param name="modifiers">Modifier keys, eg. Modifier.Ctrl | Modifier.Alt or Modifier.None</param>
+		/// <param name="key">Key to detect, eg. Key.C or Key.MediaPlayPause</param>
+		/// <param name="callback">Callback to be invoked when the keyboard combination is detected</param>
+		/// <returns>Registration status, should always be successful with this overload, dispose when done to remove the hotkey detection.</returns>
+		/// <exception cref="InvalidOperationException">Thrown when the hotkey combination could not be registered.</exception>
+		public ITinyHotKeyRegistration RegisterHotKeyOrThrow(Modifier modifiers, Key key, Func<Task> callback)
 		{
-			throw new ArgumentNullException(nameof(tinyHotKey));
-		}
+#if NET
+			ArgumentNullException.ThrowIfNull(tinyHotKey);
+#else
+			if (tinyHotKey is null)
+			{
+				throw new ArgumentNullException(nameof(tinyHotKey));
+			}
 #endif
 
-		var registration = tinyHotKey.RegisterHotKey(modifiers, key, callback);
+			var registration = tinyHotKey.RegisterHotKey(modifiers, key, callback);
 
-		if (!registration.IsRegistered)
-		{
-			throw new InvalidOperationException($"Couldn't register hotkey {modifiers} {key}");
+			if (!registration.IsRegistered)
+			{
+				throw new InvalidOperationException($"Couldn't register hotkey {modifiers} {key}");
+			}
+
+			return registration;
 		}
-
-		return registration;
 	}
 }
